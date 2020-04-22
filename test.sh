22bcbd7a61bc636e1a1b2996b4e28e40bc28a4b2
Martins Luters <luters.martins@gmail.com>
2020-04-22 12:54:56 +0300

    Add colors

--- test.sh
+++ test.sh
@@ -1,11 +1,13 @@
-RED='\033[0;32m';
+GREEN='\033[0;32m';
+RED='\033[0;31m';
 NC='\033[0m'; # No Color
 phpversion="7.3";
 userphpversion="";
-read -p "Which version of PHP you would like to use? (press enter for 7.3): " userphpversion
+echo "${RED}Which version of PHP you would like to use? (press enter for 7.3): ${NC}"
+read userphpversion
 if [ ! -z "$userphpversion" ]
 then
   phpversion="$userphpversion";
 fi
-echo "${RED}Running PHP compatibility check for PHP $phpversion ${NC}";
+echo "${GREEN}Running PHP compatibility check for PHP $phpversion ${NC}";
 ./vendor/bin/phpcs -p ./src/ --standard=PHPCompatibility --report-full=./reports/compatibility-check-full-report-"$phpversion".txt --report-summary=./reports/compatibility-check-summary-report-"$phpversion".txt --runtime-set testVersion "$phpversion" --extensions=php
