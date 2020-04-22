cfa5a75717d072dd0dde9bd18db26414ed7c09cf
Martins Luters <luters.martins@gmail.com>
2020-04-22 13:17:00 +0300

    Check if src dir is not empty before performing sniff

--- test.sh
+++ test.sh
@@ -3,6 +3,13 @@ RED='\033[0;31m';
 NC='\033[0m'; # No Color
 phpversion="7.3";
 userphpversion="";
+SRC_DIR="./src/";
+
+if [ ! "$(ls $SRC_DIR)" ]; then
+  echo "${RED}$SRC_DIR directory is empty. Nothing to sniff. ${NC}"
+  exit;
+fi
+
 echo "${RED}Which version of PHP you would like to use? (press enter for 7.3): ${NC}"
 read userphpversion
 if [ ! -z "$userphpversion" ]
@@ -10,4 +17,4 @@ then
   phpversion="$userphpversion";
 fi
 echo "${GREEN}Running PHP compatibility check for PHP $phpversion ${NC}";
-./vendor/bin/phpcs -p ./src/ --standard=PHPCompatibility --report-full=./reports/compatibility-check-full-report-"$phpversion".txt --report-summary=./reports/compatibility-check-summary-report-"$phpversion".txt --runtime-set testVersion "$phpversion" --extensions=php
+./vendor/bin/phpcs -p "$SRC_DIR" --standard=PHPCompatibility --report-full=./reports/compatibility-check-full-report-"$phpversion".txt --report-summary=./reports/compatibility-check-summary-report-"$phpversion".txt --runtime-set testVersion "$phpversion" --extensions=php
